32d0096478dcdb4e18dd316d097c0773a324993f
EduardoAmorim2 <eduardoamorim855@gmail.com>
2023-05-04 21:27:44 -0300

    Initial home body  - Added images, directory ASSETS and news Widgets in home_body(lack adjustment ).

--- weather_app/lib/home/ui/home_body.dart
+++ weather_app/lib/home/ui/home_body.dart
@@ -6,16 +6,73 @@ class HomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15.w),
-      child: Row(
-        children: [
-          Icon(Icons.local_library_outlined),
-          Text('Fortaleza'),
-          Spacer(),
-          Icon(Icons.add_alert_sharp),
-        ],
-      ),
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 5.w),
+          child: Row(
+            children: [
+              Icon(Icons.local_library_outlined),
+              SizedBox(width: 5.w),
+              Text('Fortaleza'),
+              SizedBox(width: 2.w),
+              Icon(Icons.arrow_drop_down),
+              Spacer(),
+              Icon(Icons.add_alert_sharp),
+            ],
+          ),
+        ),
+        Center(
+          child: Column(
+            children: [
+              Image.asset('assets/images/sun_cloud.png', width: 125.w),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '28º',
+                    style: TextStyle(fontSize: 30.w),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Precipitations:'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Max.: 31º'),
+                  SizedBox(width: 5.w),
+                  Text('Min.: 25º'),
+                ],
+              ),
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(20.w)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.sunny_snowing),
+                      Text('6%'),
+                      SizedBox(width: 10.w),
+                      Icon(Icons.sunny_snowing),
+                      Text('Te'),
+                      SizedBox(width: 10.w),
+                      Icon(Icons.sunny_snowing),
+                      Text('Ste'),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
