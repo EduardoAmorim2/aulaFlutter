87df89b42b5a77deff080848cd72cc7fdcce3611
EduardoAmorim2 <eduardoamorim855@gmail.com>
2023-05-16 19:12:47 -0300

    Added container  -Columns next to each other

--- weather_app/lib/home/ui/home_body.dart
+++ weather_app/lib/home/ui/home_body.dart
@@ -50,25 +50,185 @@ class HomeBody extends StatelessWidget {
                 ],
               ),
               Container(
+                height: 30.w,
+                width: 260.w,
+                padding: EdgeInsets.symmetric(horizontal: 40.w),
                 decoration: BoxDecoration(
                   color: Colors.blueAccent,
-                  borderRadius: BorderRadius.circular(20.w),
+                  borderRadius: BorderRadius.circular(12.w),
                 ),
                 child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Icon(Icons.sunny_snowing),
-                    Text('6%'),
-                    SizedBox(width: 10.w),
-                    Icon(Icons.water_drop_outlined),
-                    Text('90%'),
-                    SizedBox(width: 10.w),
-                    Icon(Icons.cloudy_snowing),
-                    Text('19Km/h'),
+                    Icon(
+                      Icons.sunny_snowing,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      '6%',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.water_drop_outlined,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      '90%',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.cloudy_snowing,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      '19Km/h',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ],
                 ),
-              )
+              ),
+              Container(
+                width: 260.w,
+                padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 5.w),
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(12.w),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Today',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                        Text(
+                          'Mar,9',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  '29ºC',
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/images/sun_cloud.png',
+                                  width: 25.w,
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  '15:00',
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  '29ºC',
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/images/sun_cloud.png',
+                                  width: 25.w,
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  '15:00',
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  '29ºC',
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/images/sun_cloud.png',
+                                  width: 25.w,
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  '15:00',
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  '29ºC',
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/images/sun_cloud.png',
+                                  width: 25.w,
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  '15:00',
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
